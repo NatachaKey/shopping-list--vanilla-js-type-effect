43c4af4ab64615813a7983d9de5b65f3898cce9a
Glitch (glitch-hello-website) <none>
2022-07-05 15:20:45 +0000

    💗🚆 Checkpoint ./script.js:42900803/3032

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const btn = document.querySelector('.btn');
 const toBuyContainer = document.querySelector('.toBuyContainer');
 
 
+
 btn.addEventListener('click', ()=>{
     const item= document.createElement('li');
     item.innerText=inputField.value;
@@ -10,17 +11,26 @@ btn.addEventListener('click', ()=>{
     item.classList.add('toBuyAdded');
     inputField.value='';
 
-    
+  
+btn.forEach((item, index) => {
+  item.addEventListener("click", () => {
+  const item= document.createElement('li');
+  item.innerText=inputField[index].value;
+  toBuyContainer[index].appendChild(item);
+  item.classList.add('toBuyAdded');
+  inputField[index].value='';
+  })})  
+  
+  
 item.addEventListener('click', ()=>{
     item.classList.add('toBuyCompleted');
 })
+  
 item.addEventListener('dblclick', ()=>{
     toBuyContainer.removeChild(item);
     
 })
 
 })
-
-
 
 
