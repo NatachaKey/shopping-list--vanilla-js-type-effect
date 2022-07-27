e935d6a3ac144b1b0611bda99d050048989b927f
Glitch (glitch-hello-website) <none>
2022-07-27 23:35:02 +0000

    📷💍 Checkpoint ./script.js:42900803/570

--- script.js
+++ script.js
@@ -3,6 +3,33 @@ const btn = document.querySelectorAll('.btn');
 const toBuyContainer = document.querySelectorAll('.toBuyContainer');
 
 
+inputField.forEach((item,index)=>{
+  
+  item.addEventListener('keypress', function(e){
+    if (e.keyCode === 13){
+      
+      const item= document.createElement('li');
+    item.innerText=inputField[index].value;
+    toBuyContainer[index].appendChild(item);
+    item.classList.add('toBuyAdded');
+    inputField[index].value='';
+      
+     item.addEventListener('click', ()=>{
+    item.classList.add('toBuyCompleted');
+    })
+  
+    item.addEventListener('dblclick', ()=>{
+    toBuyContainer[index].removeChild(item);
+    }) 
+      
+    }
+    
+  })
+})
+
+
+
+
 btn.forEach((item, index) => {
   
   item.addEventListener("click", () => {
